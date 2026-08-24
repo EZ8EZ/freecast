afa0ce79c71c10b1a04f830f26f93d46186c8d40
EZ8EZ <ericgzhang7@gmail.com>
2026-08-23 18:40:30 -0700

    Reset main to empty (pre-PR)

--- src/freecast/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Freecast: a headless, open-source batch time-series forecasting engine."""
-
-from freecast.engine import ForecastEngine, ForecastResult
-
-__version__ = "0.1.0"
-
-__all__ = ["ForecastEngine", "ForecastResult", "__version__"]
